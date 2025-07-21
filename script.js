dbc3a586f2cf265489cf4856217822aee79d3d81
Hussainb8 <mhussainb2001@hotmail.co.uk>
2025-07-21 10:56:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
+const emotionSelect = document.getElementById("emotionSelect");
+const quoteImage = document.getElementById("quoteImage");
 
-const emotionMap = {
-    "Reflective": "images/PHOTO-2025-07-17-09-20-26 2.jpg",
-    "Self-worth": "images/PHOTO-2025-06-21-16-33-18.jpg",
-    "Wisdom": "images/PHOTO-2025-06-27-00-16-15.jpg",
-    "Growth": "images/PHOTO-2025-07-03-09-09-21.jpeg",
-    "Self-awareness": "images/PHOTO-2025-07-04-12-44-00.jpg",
-    "Spiritual Awareness": "images/PHOTO-2025-07-08-09-56-57.jpg",
-    "Trauma": "images/PHOTO-2025-07-17-09-20-26.jpg"
+const imageMap = {
+  happy: "images/PHOTO-2025-07-04-12-44-00.jpg",
+  sad: "images/PHOTO-2025-07-08-09-56-57.jpg",
+  angry: "images/PHOTO-2025-07-17-09-20-26.jpg",
+  confused: "images/PHOTO-2025-07-03-09-09-21.jpeg",
+  tired: "images/PHOTO-2025-07-17-09-20-26 2.jpg",
+  overwhelmed: "images/PHOTO-2025-06-21-16-33-18.jpg",
+  calm: "images/PHOTO-2025-06-27-00-16-15.jpg"
 };
 
-document.getElementById("emotion-select").addEventListener("change", function() {
-    const emotion = this.value;
-    const img = document.getElementById("quote-image");
-    if (emotionMap[emotion]) {
-        img.src = emotionMap[emotion];
-        img.style.display = "block";
-    } else {
-        img.style.display = "none";
-    }
+emotionSelect.addEventListener("change", function () {
+  const selectedEmotion = this.value;
+  quoteImage.src = imageMap[selectedEmotion] || "";
 });
