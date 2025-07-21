22c566b43f861da9791311d2159b4aff5c9cc7e9
Hussainb8 <mhussainb2001@hotmail.co.uk>
2025-07-21 10:57:09 +0100

    Update script.

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
-const emotionSelect = document.getElementById("emotionSelect");
-const quoteImage = document.getElementById("quoteImage");
+function showQuote() {
+  const emotion = document.getElementById("emotionSelect").value;
+  const quoteImg = document.getElementById("quoteImg");
 
-const imageMap = {
-  happy: "images/PHOTO-2025-07-04-12-44-00.jpg",
-  sad: "images/PHOTO-2025-07-08-09-56-57.jpg",
-  angry: "images/PHOTO-2025-07-17-09-20-26.jpg",
-  confused: "images/PHOTO-2025-07-03-09-09-21.jpeg",
-  tired: "images/PHOTO-2025-07-17-09-20-26 2.jpg",
-  overwhelmed: "images/PHOTO-2025-06-21-16-33-18.jpg",
-  calm: "images/PHOTO-2025-06-27-00-16-15.jpg"
-};
+  const quotes = {
+    happy: "images/PHOTO-2025-07-03-09-09-21.jpeg",
+    sad: "images/PHOTO-2025-07-04-12-44-00.jpg",
+    motivated: "images/PHOTO-2025-06-21-16-33-18.jpg",
+    anxious: "images/PHOTO-2025-06-27-00-16-15.jpg",
+    tired: "images/PHOTO-2025-07-08-09-56-57.jpg",
+    grateful: "images/PHOTO-2025-07-17-09-20-26.jpg",
+    angry: "images/PHOTO-2025-07-17-09-20-26 2.jpg"
+  };
 
-emotionSelect.addEventListener("change", function () {
-  const selectedEmotion = this.value;
-  quoteImage.src = imageMap[selectedEmotion] || "";
-});
+  quoteImg.src = quotes[emotion] || "";
+}
