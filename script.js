4e1b1cff33794c3b3419f0aac711d4ed35aea9de
Hussainb8 <mhussainb2001@hotmail.co.uk>
2025-07-21 11:08:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function showQuote() {
   const quoteImg = document.getElementById("quoteImg");
 
   const quotes = {
-    happy: "images/PHOTO-2025-07-03-09-09-21.jpeg",
-    sad: "images/PHOTO-2025-07-04-12-44-00.jpg",
-    motivated: "images/PHOTO-2025-06-21-16-33-18.jpg",
-    anxious: "images/PHOTO-2025-06-27-00-16-15.jpg",
-    tired: "images/PHOTO-2025-07-08-09-56-57.jpg",
-    grateful: "images/PHOTO-2025-07-17-09-20-26.jpg",
-    angry: "images/PHOTO-2025-07-17-09-20-26 2.jpg"
+    "Reflective": "images/PHOTO-2025-07-17-09-20-26 2.jpg",
+    "Self-worth": "images/PHOTO-2025-06-21-16-33-18.jpg",
+    "Wisdom": "images/PHOTO-2025-06-27-00-16-15.jpg",
+    "Growth": "images/PHOTO-2025-07-03-09-09-21.jpeg",
+    "Self-awareness": "images/PHOTO-2025-07-04-12-44-00.jpg",
+    "Spiritual Awareness": "images/PHOTO-2025-07-08-09-56-57.jpg",
+    "Trauma": "images/PHOTO-2025-07-17-09-20-26.jpg"
   };
 
   quoteImg.src = quotes[emotion] || "";
